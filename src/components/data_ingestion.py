94d4fffc8bcd6cd26dd96722e1e36ec494a10353
muhammadawa242@gmail.com <root@Awais.localdomain>
2023-06-22 21:54:30 +0500

    data_ingestion main and pkl files

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,6 +2,8 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -43,5 +45,11 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 if __name__=="__main__":
-    obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    dt = DataTransformation()
+    train_arr, test_arr, _ = dt.initiate_data_transformation(train_data, test_data)
+    
+    model_trainer = ModelTrainer()
+    model_trainer.initiate_model_trainer(train_arr, test_arr)
